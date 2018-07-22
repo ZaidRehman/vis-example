7cb6f184ad0327c200ce481585301db47173b7ca
ZaidRehman <zqzaidqureshi@gmail.com>
2018-07-22 16:49:15 +0530

    Vis package added

--- src/graph-view.ts
+++ src/graph-view.ts
@@ -1,16 +1,16 @@
 /// <reference path="../bower_components/polymer-ts/polymer-ts.d.ts" />
-import { Network, DataSet } from 'vis/index-network'
+import { Network, DataSet } from '../vis/index-network'
 import { Car } from './Car';
 
 var drawPlay = (ctx, p, fillColor, ext = 0) => {
     var w = 25;
     var h = 25;
-    var x = p.x - w/2;
-    var y = p.y - h/2;
+    var x = p.x - w / 2;
+    var y = p.y - h / 2;
 
     //Draw triangle
     ctx.beginPath();
-    ctx.arc(p.x,p.y,12,0,2*Math.PI);
+    ctx.arc(p.x, p.y, 12, 0, 2 * Math.PI);
     ctx.moveTo(x + w / 3, y + h / 3);
     ctx.lineTo(x + w / 3, y + h - h / 3);
     ctx.lineTo(x + w - w / 4, y + h / 2);
@@ -36,8 +36,6 @@ var drawPause = (ctx, p, pauseColor, nodeColor, ext = 0) => {
     ctx.closePath();
 }
 
-
-
 var GraphView = Polymer(<any>{
     is: 'graph-view',
     properties: {
@@ -58,17 +56,47 @@ var GraphView = Polymer(<any>{
             { id: 5, label: '5', shapeColor: 'triggered' },
             { id: 6, label: '6' },
             { id: 7, label: '7', shapeColor: 'triggered' },
+            { id: 8, label: '8', shapeColor: 'triggered' },
+            // { id: 9, label: '9' },
+            // { id: 10, label: '10' },
+            // { id: 11, label: '11' },
+            // { id: 12, label: '12' },
+            // { id: 13, label: '13' },
+            // { id: 14, label: '14' },
+            // { id: 15, label: '15' },
+            // { id: 16, label: '16' },
+            // { id: 17, label: '17' },
+            
         ]);
 
         // create an array with edges
         var edges = new DataSet([
-            { from: 1, to: 3 , label: 'event', title: 'this is hover'},
-            { from: 1, to: 4 , label: 'event', title: 'this is hover'},
-            { from: 1, to: 2 , label: 'event', title: 'this is hover'},
-            { from: 2, to: 4 , label: 'event', title: 'this is hover'},
-            { from: 2, to: 5 , label: 'event', title: 'this is hover'},
-            { from: 3, to: 4 , label: 'event', title: 'this is hover'},
-            { from: 6, to: 7 , label: 'event', title: 'this is hover'},
+            { from: 1, to: 3, label: 'event', title: 'this is hover' },
+            { from: 1, to: 4, label: 'event', title: 'this is hover' },
+            { from: 1, to: 2, label: 'event', title: 'this is hover' },
+            { from: 2, to: 4, label: 'event', title: 'this is hover' },
+            { from: 2, to: 5, label: 'event', title: 'this is hover' },
+            { from: 4, to: 10, label: 'event', title: 'this is hover' },
+            { from: 3, to: 4, label: 'event', title: 'this is hover' },
+            { from: 6, to: 7, label: 'event', title: 'this is hover' },
+            { from: 7, to: 7, label: 'event', title: 'this is hover' },
+            { from: 3, to: 8, label: 'event', title: 'this is hover' },
+            { from: 8, to: 9, label: 'event', title: 'this is hover' },
+            { from: 3, to: 8 , label: 'event', title: 'this is hover'},
+            { from: 2, to: 9 , label: 'event', title: 'this is hover'},
+            { from: 3, to: 9 , label: 'event', title: 'this is hover'},
+            { from: 10, to: 11 , label: 'event', title: 'this is hover'},
+            { from: 10, to: 12 , label: 'event', title: 'this is hover'},
+            { from: 2, to: 11 , label: 'event', title: 'this is hover'},
+            { from: 10, to: 13 , label: 'event', title: 'this is hover'},
+            { from: 12, to: 16 , label: 'event', title: 'this is hover'},
+            { from: 10, to: 11 , label: 'event', title: 'this is hover'},
+            { from: 11, to: 17 , label: 'event', title: 'this is hover'},
+            { from: 10, to: 1 , label: 'event', title: 'this is hover'},
+            { from: 10, to: 15 , label: 'event', title: 'this is hover'},
+            { from: 4, to: 11 , label: 'event', title: 'this is hover'},
+            { from: 10, to: 14 , label: 'event', title: 'this is hover'},
+            { from: 8, to: 11 , label: 'event', title: 'this is hover'},
         ]);
 
         // provide the data in the vis format
@@ -77,7 +105,7 @@ var GraphView = Polymer(<any>{
             edges: edges
         };
         var options = {
-            autoResize: true,
+            // autoResize: true,
             height: '100%',
             width: '100%',
             nodes: {
@@ -111,7 +139,7 @@ var GraphView = Polymer(<any>{
                     align: 'left',
                     multi: false,
                     vadjust: 0,
-                },          
+                },
                 fixed: {
                     x: true,
                     y: true
@@ -130,7 +158,7 @@ var GraphView = Polymer(<any>{
             },
             edges: {
                 arrows: {
-                    from: { enabled: true, scaleFactor: 0.5, type: 'circle' },
+                    from: { enabled: true, scaleFactor: 0.7, type: 'circle' },
                     to: { enabled: true, scaleFactor: 0.5, type: 'arrow' }
                 },
                 arrowStrikethrough: false,
@@ -146,24 +174,28 @@ var GraphView = Polymer(<any>{
                     label: true,
                 },
                 font: {
-                    color: '#9b9b9b',
+                    color: '#232635',
                     size: 14, // px
                     face: 'Roboto',
                     //background: 'none',
                     strokeWidth: 0, // px
-                    strokeColor: '#9b9b9b',
+                    strokeColor: '#232635',
                     align: 'horizontal',
+                    //vadjust: 10,
                     multi: false,
                 },
                 hoverWidth: 5,
                 labelHighlightBold: false,
-                selectionWidth: 0,
+                selectionWidth: 1,
                 shadow: false,
                 smooth: {
                     enabled: true,
                     type: "cubicBezier",
                     roundness: .8
                 },
+                widthConstraint: {
+                    maximum: 50,
+                }
             },
             physics: {
                 enabled: false,
@@ -177,13 +209,15 @@ var GraphView = Polymer(<any>{
                 improvedLayout: true,
                 hierarchical: {
                     enabled: true,
+                    treeSpacing: 100,
+                    nodeSpacing: 250,
+                    levelSeparation: 250,
                     parentCentralization: true,
-                    levelSeparation: 150,
-                    nodeSpacing: 150,
                     direction: 'UD',
-                    sortMethod: 'directed'
+                    sortMethod: 'directed',
+                    edgeMinimization: true
                 }
-            }
+            },
         };
 
         // initialize your network!
---
+++ vis/lib/network/modules/components/nodes/shapes/Dialog.js
@@ -0,0 +1,128 @@
+'use strict';
+
+import NodeBase from '../util/NodeBase'
+
+/**
+ * A Dialog Node/Cluster shape.
+ *
+ * @extends NodeBase
+ */
+class Dialog extends NodeBase {
+  /**
+   * @param {Object} options
+   * @param {Object} body
+   * @param {Label} labelModule
+   */
+
+  //const state = ''
+  constructor(options, body, labelModule) {
+    super(options, body, labelModule);
+    this.state = options.shapeColor;
+    this._setMargins(labelModule);
+  }
+
+  /**
+   *
+   * @param {CanvasRenderingContext2D} ctx
+   * @param {boolean} [selected]
+   * @param {boolean} [hover]
+   */
+  resize(ctx, selected = this.selected, hover = this.hover) {
+    if (this.needsRefresh(selected, hover)) {
+      var dimensions = this.getDimensionsFromLabel(ctx, selected, hover);
+
+      this.width = dimensions.width + this.margin.right + this.margin.left;
+      this.height = dimensions.height + this.margin.top + this.margin.bottom;
+      this.radius = this.width / 2;
+    }
+  }
+
+  /**
+   *
+   * @param {CanvasRenderingContext2D} ctx
+   * @param {number} x width
+   * @param {number} y height
+   * @param {boolean} selected
+   * @param {boolean} hover
+   * @param {{toArrow: boolean, toArrowScale: (allOptions.edges.arrows.to.scaleFactor|{number}|allOptions.edges.arrows.middle.scaleFactor|allOptions.edges.arrows.from.scaleFactor|Array|number), toArrowType: *, middleArrow: boolean, middleArrowScale: (number|allOptions.edges.arrows.middle.scaleFactor|{number}|Array), middleArrowType: (allOptions.edges.arrows.middle.type|{string}|string|*), fromArrow: boolean, fromArrowScale: (allOptions.edges.arrows.to.scaleFactor|{number}|allOptions.edges.arrows.middle.scaleFactor|allOptions.edges.arrows.from.scaleFactor|Array|number), fromArrowType: *, arrowStrikethrough: (*|boolean|allOptions.edges.arrowStrikethrough|{boolean}), color: undefined, inheritsColor: (string|string|string|allOptions.edges.color.inherit|{string, boolean}|Array|*), opacity: *, hidden: *, length: *, shadow: *, shadowColor: *, shadowSize: *, shadowX: *, shadowY: *, dashes: (*|boolean|Array|allOptions.edges.dashes|{boolean, array}), width: *}} values
+   */
+  draw(ctx, x, y, selected, hover, values) {
+    this.resize(ctx, selected, hover);
+    this.left = x - this.width / 2;
+    this.top = y - this.height / 2;
+
+    this.initContextForDraw(ctx, values);
+
+    var headColor = '';
+    switch (this.state) {
+      case 'accepted': headColor = '#70DC76'; break;
+      case 'rejected': headColor = '#FF4200'; break;
+      case 'triggered': headColor = '#5C6BC0'; break;
+      default: headColor = '#F4F4F6'
+    }
+    var radius = 4;
+    var barHeight = 5;
+
+
+    ctx.beginPath();
+    ctx.strokeStyle = '#F4F4F6';
+    ctx.fillStyle = '#F4F4F6'
+    ctx.roundRect(this.left, this.top, this.width, this.height, radius);
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = headColor;
+    ctx.fillStyle = headColor;
+
+    ctx.moveTo(this.left + radius, this.top);
+    ctx.lineTo(this.left + this.width - radius, this.top);
+
+    ctx.quadraticCurveTo(this.left + this.width, this.top , this.left + this.width, this.top + radius);
+    ctx.lineTo(this.left + this.width, this.top + barHeight)
+    ctx.lineTo(this.left,this.top + barHeight)
+    ctx.lineTo(this.left,this.top + radius)
+    ctx.quadraticCurveTo(this.left, this.top, this.left + radius, this.top);
+
+    ctx.fill()
+    ctx.closePath();
+    ctx.stroke();
+
+    this.updateBoundingBox(x, y, ctx, selected, hover);
+    this.labelModule.draw(ctx, this.left + this.textSize.width / 2 + this.margin.left,
+      this.top + this.textSize.height / 2 + this.margin.top, selected, hover);
+  }
+
+  /**
+   *
+   * @param {number} x width
+   * @param {number} y height
+   * @param {CanvasRenderingContext2D} ctx
+   * @param {boolean} selected
+   * @param {boolean} hover
+   */
+  updateBoundingBox(x, y, ctx, selected, hover) {
+    this._updateBoundingBox(x, y, ctx, selected, hover);
+
+    let borderRadius = this.options.shapeProperties.borderRadius; // only effective for box
+    this._addBoundingBoxMargin(borderRadius);
+  }
+
+  /**
+   *
+   * @param {CanvasRenderingContext2D} ctx
+   * @param {number} angle
+   * @returns {number}
+   */
+  distanceToBorder(ctx, angle) {
+    this.resize(ctx);
+    let borderWidth = this.options.borderWidth;
+
+    return Math.min(
+      Math.abs((this.width) / 2 / Math.cos(angle)),
+      Math.abs((this.height) / 2 / Math.sin(angle))) + borderWidth;
+  }
+}
+
+export default Dialog;
